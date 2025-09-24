b21361840c74ebd90ef5dee2ccd40e961dd5a978
muhameds4eed <mase65ed@gmail.com>
2025-09-24 22:40:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// This was the old line:
+// const anniversaryStartDate = new Date("2023-01-15T12:00:00");
+
+// This is the new line:
+const anniversaryStartDate = new Date("2024-09-19T00:00:00");
